a9ee3fca988a417aa80e4920b3c0e87bd1a5ac0b
JunSeok <kkuld21@gmail.com>
2021-07-22 16:07:36 +0900

    ㅇ

---
+++ js/colors.js
@@ -0,0 +1,37 @@
+var Body = {
+  setColor:function(color){
+    // document.querySelector('body').style.color= color;
+    $('body').css('color',color);
+  },
+  setBackgroundColor:function(color){
+    // document.querySelector('body').style.backgroundColor= color;
+    $('body').css('backgroundColor',color);
+  }
+}
+
+var Links = {
+  setColor:function(color){
+    // var links = document.querySelectorAll('a');
+    // var i = 0;
+    // while(i < links.length){
+    //   links[i].style.color = color;
+    //   i += 1;
+    // }
+    $('a').css('color', color);
+  }
+}
+
+
+function nightDayHandler(self){
+  if(self.value == 'night'){
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
+    self.value = 'day';
+    Links.setColor('powderblue');
+  } else {
+    Body.setBackgroundColor('white');
+    Body.setColor('black');
+    self.value = 'night';
+    Links.setColor('blue');
+  }
+}
